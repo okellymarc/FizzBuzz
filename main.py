79100c17e212f25d8c42dfd6e45f4563f386322d
okellymarc <okellymarc@gmail.com>
2022-10-09 09:05:13 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# If a number is divisible by 3 write "Fizz" if it's divisible by 5 write "Buzz". If neither write "__"
+number_set = [3, 4, 55, 36, 12, 1, 100, 27, 45, 2, 360, 22, 150]
+
+for number in number_set:
+    if number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print("__")
+# Fizz buzz appears to be working correctly
